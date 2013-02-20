f31d8c942c4ea5a2cac378b257741cbd504420c8
Александр Шилин <a.shilin@analit.net>
2013-02-20 11:27:47 +0400

    Добавил исходники проекта StyleCopAddOn - дополнительных правил для проверки форматирования кода

---
+++ projects/StyleCopAddOn/src/StyleCopAddOn/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("StyleCopAddOn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StyleCopAddOn")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2785bb48-d3ac-431b-8139-8a4e160dd08f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер верси
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию,
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ projects/StyleCopAddOn/src/StyleCopAddOn/StyleCopAddOn.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StyleCop;
+using StyleCop.CSharp;
+using System.Windows.Forms;
+
+namespace StyleCopAddOn
+{
+	[SourceAnalyzer(typeof(CsParser))]
+	public class StyleCopAddOn : SourceAnalyzer
+	{
+		private const int _tabSpaces = 1;
+
+		public override void AnalyzeDocument(CodeDocument document)
+		{
+			CsDocument csharpDocument = (CsDocument)document;
+			if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated) {
+				csharpDocument.WalkDocument(
+					new CodeWalkerElementVisitor<object>(this.VisitElement),
+					new CodeWalkerStatementVisitor<object>(this.VisitStatement),
+					new CodeWalkerExpressionVisitor<object>(this.VisitExpression));
+			}
+
+			base.AnalyzeDocument(document);
+		}
+
+		private bool CheckPreviousTokens(CsElement parentElement, Expression expression, int lastRow, int curentPosition)
+		{
+			return expression.Tokens.Count(t => t.Location.StartPoint.LineNumber > expression.LineNumber
+				&& t.Location.StartPoint.LineNumber < lastRow
+				&& GetMinIndex(parentElement, t.Location.StartPoint.LineNumber) == curentPosition - 1) > 0;
+		}
+
+		private void ExpressionWalk(Expression expression, CsElement parentElement)
+		{
+			foreach (var child in expression.ChildExpressions) {
+				if (child.Tokens.Count(t => t.CsTokenType == CsTokenType.String
+					&& t.Location.StartPoint.LineNumber != t.Location.EndPoint.LineNumber) == 0 &&
+					expression.Tokens.Count(t => t.CsTokenType == CsTokenType.String
+						&& t.Location.StartPoint.LineNumber != t.Location.EndPoint.LineNumber) == 0) {
+					if (child.LineNumber != expression.LineNumber) {
+						var minIndex = parentElement.Tokens.Where(e => e.LineNumber == child.LineNumber
+							&& e.CsTokenType != CsTokenType.WhiteSpace).Min(t => t.Location.StartPoint.IndexOnLine);
+						if (parentElement.Tokens.FirstOrDefault(t => t.LineNumber == child.LineNumber
+							&& t.Location.StartPoint.IndexOnLine == GetMinIndex(parentElement, child.LineNumber))
+							.CsTokenType != CsTokenType.CloseCurlyBracket)
+							if (minIndex - GetMinIndex(parentElement, expression.LineNumber) != _tabSpaces) {
+								var tok = expression.Tokens.FirstOrDefault(t => t.Location.StartPoint.LineNumber == child.LineNumber);
+								if (expression.ExpressionType == ExpressionType.MethodInvocation)
+									if (CheckPreviousTokens(parentElement, expression, child.LineNumber, GetMinIndex(parentElement, child.LineNumber)))
+										continue;
+								this.AddViolation(parentElement, child.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+
+
+								continue;
+							}
+					}
+					ExpressionWalk(child, parentElement);
+				}
+			}
+		}
+
+		private void StatementWalk(Statement statement, CsElement parentElement)
+		{
+			foreach (var child in statement.ChildStatements) {
+				if (child.LineNumber != statement.LineNumber && child.StatementType != StatementType.Block) {
+					if (GetMinIndex(parentElement, child.LineNumber) - GetMinIndex(parentElement, statement.LineNumber) != _tabSpaces && statement.StatementType != StatementType.Block) {
+						this.AddViolation(parentElement, child.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+						return;
+					}
+
+					if (GetMinIndex(parentElement, child.LineNumber) - GetMinIndex(parentElement, statement.Parent.LineNumber) != _tabSpaces && statement.StatementType == StatementType.Block) {
+						this.AddViolation(parentElement, child.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+						return;
+					}
+				}
+				StatementWalk(child, parentElement);
+			}
+		}
+
+		private bool VisitExpression(Expression expression, Expression parentExpression, Statement parentStatement, CsElement parentElement, object context)
+		{
+			if (parentExpression == null) {
+				if (parentStatement != null) {
+					if (parentElement.Tokens.FirstOrDefault(t => t.LineNumber == expression.LineNumber && t.CsTokenType != CsTokenType.WhiteSpace
+						&& t.Location.StartPoint.IndexOnLine == GetMinIndex(parentElement, expression.LineNumber)).CsTokenType != CsTokenType.CloseCurlyBracket)
+						if (parentStatement.LineNumber != expression.LineNumber &&
+							GetMinIndex(parentElement, expression.LineNumber) - GetMinIndex(parentElement, parentStatement.LineNumber) != _tabSpaces) {
+							if (parentStatement.StatementType != StatementType.DoWhile) {
+								this.AddViolation(parentElement, expression.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+							}
+							else {
+								if (parentStatement.Location.EndPoint.LineNumber != expression.LineNumber &&
+									GetMinIndex(parentElement, expression.LineNumber) - GetMinIndex(parentElement, parentStatement.LineNumber) != _tabSpaces) {
+									this.AddViolation(parentElement, expression.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+								}
+							}
+							return true;
+						}
+				}
+				ExpressionWalk(expression, parentElement);
+			}
+			var curly = expression.Tokens.LastOrDefault(t => t.CsTokenType == CsTokenType.CloseCurlyBracket);
+			if (curly != null && curly.Location.StartPoint.IndexOnLine != GetMinIndex(parentElement, expression.LineNumber) && curly.Location.StartPoint.IndexOnLine != GetMinIndex(parentElement, expression.Parent.LineNumber)
+				&& curly.LineNumber != expression.LineNumber && curly.Parent.Equals(expression)) {
+				this.AddViolation(parentElement, curly.LineNumber, "CloseCurlyBraketMustBeOnTheSameColumn");
+			}
+			curly = expression.Tokens.FirstOrDefault(t => t.CsTokenType == CsTokenType.OpenCurlyBracket);
+
+			if (curly != null && curly.Parent.Parent == expression) {
+				if (expression.LineNumber != curly.LineNumber && curly.Location.StartPoint.IndexOnLine == GetMinIndex(parentElement, curly.LineNumber)) {
+					this.AddViolation(parentElement, curly.LineNumber, "OpenCurlyBraketOnTheSameLine");
+				}
+			}
+			return true;
+		}
+
+		private bool VisitStatement(Statement statement, Expression parentExpression, Statement parentStatement, CsElement parentElement, object context)
+		{
+			if (parentStatement == null) {
+				var minIndex = parentElement.Tokens.Where(e => e.LineNumber == statement.LineNumber
+					&& e.CsTokenType != CsTokenType.WhiteSpace).Min(t => t.Location.StartPoint.IndexOnLine);
+				if (parentElement.Tokens.FirstOrDefault(t => t.LineNumber == statement.LineNumber && t.CsTokenType != CsTokenType.WhiteSpace
+					&& t.Location.StartPoint.IndexOnLine == GetMinIndex(parentElement, statement.LineNumber)).CsTokenType != CsTokenType.CloseCurlyBracket)
+
+					if (statement.LineNumber != parentElement.LineNumber &&
+						minIndex - GetMinIndex(parentElement, parentElement.LineNumber) != _tabSpaces) {
+						this.AddViolation(parentElement, statement.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+						return true;
+					}
+				StatementWalk(statement, parentElement);
+			}
+			if (statement.StatementType == StatementType.Block) {
+				if (parentStatement != null && parentStatement.StatementType == StatementType.Else && ((ElseStatement)parentStatement).ConditionExpression == null
+					&& statement.ChildStatements.Where(s => s.StatementType != StatementType.If && s.StatementType != StatementType.Else).Count() == 0) {
+					if (statement.ChildStatements.Where(s => s.StatementType == StatementType.If).Count() <= 1) {
+						this.AddViolation(parentElement, statement.LineNumber, "ElseIfStatementMustBeWithoutBlock");
+						return true;
+					}
+				}
+				if (parentStatement != null && parentStatement.Tokens.Where(t => t.LineNumber == statement.LineNumber &&
+					t.CsTokenType != CsTokenType.WhiteSpace &&
+					t.Location.StartPoint.IndexOnLine < statement.Location.StartPoint.IndexOnLine).ToList().Count == 0) {
+					this.AddViolation(parentElement, statement.LineNumber, "OpenCurlyBraketOnTheSameLine");
+				}
+			}
+			return true;
+		}
+
+		private bool VisitElement(CsElement element, CsElement parentElement, object context)
+		{
+			if (parentElement != null && parentElement.ElementType != ElementType.Root) {
+				if (parentElement.Tokens.FirstOrDefault(t => t.LineNumber == element.LineNumber && t.CsTokenType != CsTokenType.WhiteSpace).CsTokenType != CsTokenType.CloseCurlyBracket)
+					if (element.Location.StartPoint.LineNumber != parentElement.Location.StartPoint.LineNumber &&
+						GetMinIndex(parentElement, element.LineNumber) - GetMinIndex(parentElement.Parent as CsElement, parentElement.LineNumber) != _tabSpaces) {
+						this.AddViolation(parentElement, element.LineNumber, "MoreOrLessThenOneTabToRightPosition");
+					}
+			}
+			return true;
+		}
+
+		private int GetMinIndex(CsElement element, int lineNumber)
+		{
+			var res = element.Tokens.Where(e => e.LineNumber == lineNumber
+				&& e.CsTokenType != CsTokenType.WhiteSpace).Min(t => t.Location.StartPoint.IndexOnLine);
+			return res;
+		}
+	}
+}
