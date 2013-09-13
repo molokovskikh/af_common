ad227e8abd61690dffdebf0182e2e9460815f8a3
Квасов Роман <r.kvasov@analit.net>
2013-09-13 13:16:18 +0400

    поправил путь в скрипте

--- scripts/project.sh
+++ scripts/project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 name=$1
-root=/cygdrive/c/Projects/Production/templates
+root=/cygdrive/c/Users/kvasov/projects/Production/templates
 packages=(nunit nhibernate mysql.data microsoft.aspnet.webapi log4net stylecop.msbuild)
 warmup.0.6.5.0/bin/warmup.exe base $name
 cd $name
