3aacf68539ab0b8100098e195e68da7f2d717cc3
Квасов Роман <r.kvasov@analit.net>
2013-10-08 12:54:40 +0400

    jenkins-job-script - улучшил механизм обновления репозитория

--- scripts/jenkins-job-script.sh
+++ scripts/jenkins-job-script.sh
@@ -2,12 +2,15 @@
 
 PATH=/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319/:$PATH
 
-mysqladmin --user=root --port=$(cat data/port) shutdown 2> /dev/null && sleep 2 || :
+if [ -n "SKIP_DB" ]; then
+	mysqladmin --user=root --port=$(cat data/port) shutdown 2> /dev/null && sleep 2 || :
+fi
 git clean -fdx
 git submodule foreach git clean -fdx
 
 if [ -n "$AUTO_UPDATE" ]
 then
+	git pull
 	git checkout master
 	git submodule sync
 	git submodule update --init
@@ -16,19 +19,25 @@ then
 	git submodule foreach git clean -fdx
 fi
 
-bake RunMySql path=data randomPort=true notInteractive=true
-port=$(cat data/port)
-grep "(Data Source|server)=localhost" src -lRP | xargs perl -i -pe "s/connectionString=\"([^\"]*)?port=\d+;([^\"]*)?\"/connectionString=\"port="$port";\1\2\"/gi"
-grep "(Data Source|server)=localhost" src -lRP | xargs perl -i -pe 's/(Data Source|server)=localhost/Data Source=localhost;port='$port'/gi'
+if [ -n "SKIP_DB" ]; then
+	bake RunMySql path=data randomPort=true notInteractive=true
+	port=$(cat data/port)
+	grep "(Data Source|server)=localhost" src -lRP | xargs perl -i -pe "s/connectionString=\"([^\"]*)?port=\d+;([^\"]*)?\"/connectionString=\"port="$port";\1\2\"/gi"
+	grep "(Data Source|server)=localhost" src -lRP | xargs perl -i -pe 's/(Data Source|server)=localhost/Data Source=localhost;port='$port'/gi'
+fi
 
 if [ -e ./scripts/prepare.sh ]
 then
 	./scripts/prepare.sh
 else
-	bake packages:install notInteractive=true | iconv -f cp1251 -t utf-8
+	bake packages:install notInteractive=true | iconv -f cp866 -t cp1251
 fi
-bake TryToBuild db:setup test Port=$port notInteractive=true | iconv -f cp1251 -t utf-8
+bake TryToBuild Port=$port notInteractive=true | iconv -f cp866 -t cp1251
+bake db:setup Port=$port notInteractive=true | iconv -f cp866 -t cp1251
+bake test Port=$port notInteractive=true | iconv -f cp866 -t cp1251
 
 git checkout .
 git submodule foreach git checkout .
-mysqladmin --user=root --port=$port shutdown
+if [ -n "SKIP_DB" ]; then
+	mysqladmin --user=root --port=$port shutdown
+fi
