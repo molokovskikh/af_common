4bd1a0cecdaf9a814c67b5b932ba8e628493b713
Квасов Роман <r.kvasov@analit.net>
2013-10-08 13:08:22 +0400

    jenkis-job-script - исправил опцию игнорирования создания базы данных

--- scripts/jenkins-job-script.sh
+++ scripts/jenkins-job-script.sh
@@ -2,7 +2,7 @@
 
 PATH=/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319/:$PATH
 
-if [ -n "SKIP_DB" ]; then
+if [ -z "SKIP_DB" ]; then
 	mysqladmin --user=root --port=$(cat data/port) shutdown 2> /dev/null && sleep 2 || :
 fi
 git clean -fdx
@@ -10,8 +10,8 @@ git submodule foreach git clean -fdx
 
 if [ -n "$AUTO_UPDATE" ]
 then
-	git pull
 	git checkout master
+	git pull
 	git submodule sync
 	git submodule update --init
 	git submodule foreach "git checkout master && git pull"
@@ -19,7 +19,7 @@ then
 	git submodule foreach git clean -fdx
 fi
 
-if [ -n "SKIP_DB" ]; then
+if [ -z "SKIP_DB" ]; then
 	bake RunMySql path=data randomPort=true notInteractive=true
 	port=$(cat data/port)
 	grep "(Data Source|server)=localhost" src -lRP | xargs perl -i -pe "s/connectionString=\"([^\"]*)?port=\d+;([^\"]*)?\"/connectionString=\"port="$port";\1\2\"/gi"
@@ -33,13 +33,13 @@ else
 	bake packages:install notInteractive=true
 fi
 bake TryToBuild Port=$port notInteractive=true
-if [ -n "SKIP_DB" ]; then
+if [ -z "SKIP_DB" ]; then
 	bake db:setup Port=$port notInteractive=true
 fi
 bake test Port=$port notInteractive=true
 
 git checkout .
 git submodule foreach git checkout .
-if [ -n "SKIP_DB" ]; then
+if [ -z "SKIP_DB" ]; then
 	mysqladmin --user=root --port=$port shutdown
 fi
