d660ab574094b8c753c500a8d2e3e2e396166622
Квасов Роман <r.kvasov@analit.net>
2014-01-10 18:27:34 +0400

    Улучшил скрипт для исправления ссылок в файле проекта

--- scripts/fix-ref.sh
+++ scripts/fix-ref.sh
@@ -2,6 +2,6 @@
 
 for f in `find -iname *.csproj`
 do
-perl -i -pe "s/<Reference\sInclude=\"([\w\.]+)(,[\w\d\s, =\.]*)\"?/<Reference Include=\"\1\"/" $f
+perl -i -pe "s/<Reference\sInclude=\"([\w\d\.-]+)(,[\w\d\s, =\.]*)\"?/<Reference Include=\"\1\"/" $f
 find -iname *.bak | xargs rm
 done
