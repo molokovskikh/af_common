9d5c0cbfd6542398a3f9b54e9f7d75004dc1f988
Квасов Роман <r.kvasov@analit.net>
2014-01-14 09:35:09 +0400

    Исправил автообновление

--- scripts/jenkins-global-update.sh
+++ scripts/jenkins-global-update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl "http://devsrv.adc.analit.net:8080/view/root/api/json?tree=jobs\[name\]" | /bin/grep -oP ":\"([\w\.])+\"" | /bin/grep -oP "[\w\.]+" | tr [A-Z] [a-z] | tr . - | xargs -I{} curl "http://devsrv.adc.analit.net:8080/job/update-job/buildWithParameters?NAME="{}
+curl "http://devsrv.adc.analit.net:8080/view/root/api/json?tree=jobs\[name\]" | /bin/grep -oP ":\"([\w\.-])+\"" | /bin/grep -oP "[\w\.-]+" | tr [A-Z] [a-z] | tr . - | xargs -I{} curl "http://devsrv.adc.analit.net:8080/job/update-job/buildWithParameters?NAME="{}
