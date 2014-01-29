297b74e3d5d0099191148eb99cce52e301859b26
Квасов Роман <r.kvasov@analit.net>
2014-01-29 09:54:00 +0400

    Исправил скрипт для обновления javascript

--- scripts/fix-js.sh
+++ scripts/fix-js.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-for f in `find src test -iname '*.js' -or -iname '*.coffee'`
-do
-	perl -i -pe "s/\)\.live\(/).on(/" $f
-done
-find -iname *.bak | xargs rm
-grunt coffee
+if [ -f Gruntfile.js ]; then
+	for f in `find src test -iname '*.js' -or -iname '*.coffee'`
+	do
+		perl -i -pe "s/\)\.live\(/).on(/" $f
+	done
+	find -iname *.bak | xargs rm
+	grunt coffee
+fi
