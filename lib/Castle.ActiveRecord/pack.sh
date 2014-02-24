7ca7a95aebc62417bc85aaa3dc5e86491bdc35f1
Квасов Роман <r.kvasov@analit.net>
2014-02-24 09:42:09 +0400

    Обновил Castle.ActiveRecord

--- lib/Castle.ActiveRecord/pack.sh
+++ lib/Castle.ActiveRecord/pack.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-nuget pack
-mv *.nupkg ../../nuget/
+if [ ! -d active-record ]; then
+git clone git@git.analit.net:r.kvasov/active-record.git
+fi
+cd ./active-record/
+find -iname packages.config | xargs -I{} nuget install -OutputDirectory packages {}
+./build.cmd
+cd ..
+cp build/NET40/NET40-Release/bin/Castle.ActiveRecord.* lib/net40/
+rm lib/net40/*.Tests.*
+nuget pack -Verbose Package.nuspec -Exclude '**\active-record\**' -Exclude pack.sh
+mv *.nupkg ../../nuget
