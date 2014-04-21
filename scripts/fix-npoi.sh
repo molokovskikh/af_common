3e28f708a68e93bd0744630ce56c9fc41da41a2d
Квасов Роман <r.kvasov@analit.net>
2014-04-21 09:46:17 +0400

    Улучшил скрипт для миграции npoi

--- scripts/fix-npoi.sh
+++ scripts/fix-npoi.sh
@@ -3,5 +3,11 @@
 for f in `find -iname '*.cs'`
 do
 	perl -i -pe "s/CellType\.NUMERIC/CellType\.Numeric/" $f
+	perl -i -pe "s/CellType\.BLANK/CellType\.Blank/" $f
+	perl -i -pe "s/CellType\.BOOLEAN/CellType\.Boolean/" $f
+	perl -i -pe "s/BorderStyle\.MEDIUM/BorderStyle\.Medium/" $f
+	perl -i -pe "s/BorderStyle\.THIN/BorderStyle\.Thin/" $f
+	perl -i -pe "s/HorizontalAlignment\.CENTER/HorizontalAlignment\.Center/" $f
+	perl -i -pe "s/HorizontalAlignment\.RIGHT/HorizontalAlignment\.Right/" $f
 done
 find -iname *.bak | xargs rm
