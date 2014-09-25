5c73a806e765a3f7c42d31c4c8b0040aa6a8e003
Квасов Роман <r.kvasov@analit.net>
2014-09-25 12:04:35 +0400

    Добавил скрипт для упаковки Castle.Components.Binder

---
+++ lib/Castle.Components.Binder/pack.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+name=binder
+url=git@github.com:radiy/Castle.Components.Binder.git
+mask=Castle.Components.Binder
+
+if [ ! -d $name ]; then
+git clone $url $name
+fi
+cd ./$name/
+find -iname packages.config | xargs -I{} nuget install -OutputDirectory packages {}
+chmod +x build.cmd
+./build.cmd
+find -iname '*.exe' | xargs chmod +x
+find -iname '*.dll' | xargs chmod +x
+cd ..
+rm -r lib/net40/*
+cp ./$name/build/NET40/NET40-Release/bin/$mask.* lib/net40/
+cp -r ./$name/build/NET40/NET40-Release/bin/ru lib/net40/
+find lib -iname '*.Tests.*' | xargs rm
+nuget pack -Verbose Package.nuspec -Exclude '**\'$name'\**' -Exclude pack.sh
+mv *.nupkg ../../nuget
