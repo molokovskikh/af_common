c1ce3bc81c4d4deb11c8416996b7b872faab3ace
Квасов Роман <r.kvasov@analit.net>
2014-10-30 11:55:19 +0400

    Исправил скрипт для обновления конфигурации jenkins

--- scripts/jenkins-update-config.sh
+++ scripts/jenkins-update-config.sh
@@ -1,1 +1,4 @@
-curl -X POST http://devsrv.adc.analit.net:8080/job/$1/config.xml --data-binary "@config.xml" -H "Content-Type: text/xml"
+name=`echo $1 | tr 'A-Z' 'a-z'`
+cat config.xml | sed "s/\$JOB_NAME/$name/" > tmp_config
+curl -X POST http://devsrv.adc.analit.net:8080/job/$1/config.xml --data-binary "@tmp_config" -H "Content-Type: text/xml"
+rm tmp_config
