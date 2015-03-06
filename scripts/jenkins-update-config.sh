2c0c10e28852ff4fc9f2de63f56c73b5d0d00bd0
Квасов Роман <r.kvasov@analit.net>
2015-03-06 10:14:37 +0300

    Jenkins: обновил конфигурационный файл по умолчанию
    Не рассылаю письма о сборке всем подряд

--- scripts/jenkins-update-config.sh
+++ scripts/jenkins-update-config.sh
@@ -1,3 +1,7 @@
+if [ -z "$1" ]; then
+	echo "Не задано имя проекта для получения конфигурации - $0 <имя проекта>"
+	exit
+fi
 name=`echo $1 | tr 'A-Z' 'a-z'`
 cat config.xml | sed "s/\$JOB_NAME/$name/" > tmp_config
 curl -X POST http://devsrv.adc.analit.net:8080/job/$1/config.xml --data-binary "@tmp_config" -H "Content-Type: text/xml"
