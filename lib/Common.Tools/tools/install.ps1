dd6fdb60ca7fec7a3642cff0cc2a19fb63cce2c1
Квасов Роман <r.kvasov@analit.net>
2015-08-07 12:05:35 +0300

    Сделал скрипт для установки сабмодуля универсальных

--- lib/Common.Tools/tools/install.ps1
+++ lib/Common.Tools/tools/install.ps1
@@ -1,11 +1,12 @@
-param($installPath, $toolsPath, $package, $project)
+param($installPath, $toolsPath, $package, $project, $name="common-tools")
 
 $git="git.exe"
 if (Test-Path "C:\cygwin\bin\git.exe") {
 	$git="C:\cygwin\bin\git.exe"
 }
 mkdir lib -erroraction 'silentlycontinue'
-invoke-expression "$git submodule add git@git.analit.net:root/common-tools.git lib/common.tools"
+invoke-expression "$git submodule add git@git.analit.net:root/$name.git lib/$name"
 invoke-expression "$git submodule update --init"
-$path = "lib/common.tools/common.tools/common.tools.csproj" | resolve-path
+$localname = $name -replace "-", "."
+$path = "lib/$name/$localName/$localName.csproj" | resolve-path
 $dte.solution.addfromfile($path)
