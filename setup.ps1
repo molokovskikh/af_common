8d24b16823f3a9438abc90cda042abf12e0f64fd
Квасов Роман <r.kvasov@analit.net>
2016-04-18 17:42:17 +0300

    Добавил скрипт для установки системы

---
+++ setup.ps1
@@ -0,0 +1,9 @@
+choco install mysql.workbench
+choco install notepadplusplus
+choco install 7zip
+choco install googlechrome
+choco install sysinternals
+choco install visualstudio2015community
+choco install consolez
+cyg-get wget
+cyg-get nano
