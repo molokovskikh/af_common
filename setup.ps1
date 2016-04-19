0665e01842a93986e01e7dde7ca8c5ba81683b83
Квасов Роман <r.kvasov@analit.net>
2016-04-19 12:02:22 +0300

    Доработал скрипт подготовки

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 choco feature enable -n=allowGlobalConfirmation
 choco install mysql.workbench
 choco install notepadplusplus
@@ -8,6 +9,7 @@ choco install nuget.commandline
 choco install visualstudio2015community
 choco install consolez
 choco install ilspy
+choco install resharper-platform -version 104.0.20151218.134438
 cyg-get wget nano
 if ( -Not (Test-Path "C:\tools\mysql" ))
 {
@@ -15,14 +17,42 @@ if ( -Not (Test-Path "C:\tools\mysql" ))
  C:\tools\mysql\bin\mysqld.exe --install
  net start mysql
 }
-bash --login -c git config --global color.branch auto
-bash --login -c git config --global color.diff auto
-bash --login -c git config --global color.interactive auto
-bash --login -c git config --global color.status auto
-bash --login -c git config --global core.whitespace trailing-space,space-before-tab,cr-at-eol
-bash --login -c git config --global core.editor nano
-cp -r "\\offdc\MMedia\Московский офис\Файлы для установки системы (развертывания проекта с нуля)\файлы настроек\.bashrc" "C:\tools\cygwin\home\$env:USERNAME\.bashrc"
+C:\tools\cygwin\bin\bash --login -c "git config --global color.branch auto"
+C:\tools\cygwin\bin\bash --login -c "git config --global color.diff auto"
+C:\tools\cygwin\bin\bash --login -c "git config --global color.interactive auto"
+C:\tools\cygwin\bin\bash --login -c "git config --global color.status auto"
+C:\tools\cygwin\bin\bash --login -c "git config --global core.whitespace trailing-space,space-before-tab,cr-at-eol"
+C:\tools\cygwin\bin\bash --login -c "git config --global core.editor nano"
+cp $env:USERPROFILE\projects\common\etc\.bashrc "C:\tools\cygwin\home\$env:USERNAME\.bashrc"
 if ( -Not (Test-Path $env:USERPROFILE\AppData\Roaming\Console\)) {
     mkdir $env:USERPROFILE\AppData\Roaming\Console\
 }
-cp $env:USERPROFILE\projects\common\Console.xml $env:USERPROFILE\AppData\Roaming\Console\Console.xml
+cp $env:USERPROFILE\projects\common\etc\Console.xml $env:USERPROFILE\AppData\Roaming\Console\Console.xml
+$sources=nuget source
+$sourceExists=$false
+foreach ($line in $sources) {
+	$sourceExists=$line.ToLower().IndexOf("common\nuget\") -ge 0
+	if ($sourceExists) {
+		break
+	}
+}
+if (-Not $sourceExists) {
+	nuget source add -Name local -Source $env:USERPROFILE/projects/common/nuget/
+}
+C:\tools\cygwin\bin\bash --login -c '/cygdrive/c/users/$USER/projects/common/install'
+
+function curlex($url, $filename) {
+  $path = [io.path]::gettemppath() + "\" + $filename
+  if( test-path $path ) { rm -force $path }
+  (new-object net.webclient).DownloadFile($url, $path)
+
+  return new-object io.fileinfo $path
+}
+
+function installsilently($url, $name) {
+  echo "Installing $name"
+  $extension = (curlex $url $name).FullName
+  $result = Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" -ArgumentList "/q $extension" -Wait -PassThru;
+}
+
+installsilently https://visualstudiogallery.msdn.microsoft.com/c8bccfe2-650c-4b42-bc5c-845e21f96328/file/75539/12/EditorConfigPlugin.vsix EditorConfigPlugin.vsix
