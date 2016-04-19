27a4fa562a3da384fa6b7a5ad163b8fd3d7f0c56
Квасов Роман <r.kvasov@analit.net>
2016-04-19 10:19:43 +0300

    Доработал скрипт подготовки

--- setup.ps1
+++ setup.ps1
@@ -4,10 +4,11 @@ choco install notepadplusplus
 choco install 7zip
 choco install googlechrome
 choco install sysinternals
+choco install nuget.commandline
 choco install visualstudio2015community
 choco install consolez
-cyg-get wget
-cyg-get nano
+choco install ilspy
+cyg-get wget nano
 if ( -Not (Test-Path "C:\tools\mysql" ))
 {
  cp -r "\\offdc\MMedia\Московский офис\Файлы для установки системы (развертывания проекта с нуля)\файлы настроек\apps\MySQL\mysql-5.6.14-winx64" "C:\tools\mysql"
@@ -21,3 +22,7 @@ bash --login -c git config --global color.status auto
 bash --login -c git config --global core.whitespace trailing-space,space-before-tab,cr-at-eol
 bash --login -c git config --global core.editor nano
 cp -r "\\offdc\MMedia\Московский офис\Файлы для установки системы (развертывания проекта с нуля)\файлы настроек\.bashrc" "C:\tools\cygwin\home\$env:USERNAME\.bashrc"
+if ( -Not (Test-Path $env:USERPROFILE\AppData\Roaming\Console\)) {
+    mkdir $env:USERPROFILE\AppData\Roaming\Console\
+}
+cp $env:USERPROFILE\projects\common\Console.xml $env:USERPROFILE\AppData\Roaming\Console\Console.xml
