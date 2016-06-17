6179f8a5de68d033f13119a0e54d074307163132
Квасов Роман <r.kvasov@analit.net>
2016-06-17 13:33:11 +0300

    Добавит скрипт обновления nunit

--- scripts/fix-nunit.sh
+++ scripts/fix-nunit.sh
@@ -5,4 +5,9 @@ do
 	perl -i -pe 's/\[SetUp\]/\[OneTimeSetUp\]/' $f
 	perl -i -pe 's/\[TearDown\]/\[OneTimeTearDown\]/' $f
 done
+for f in `git ls-files | grep \.cs | xargs grep -l 'NUnit.Framework'`
+do
+	perl -i -pe 's/Is\.StringContaining/Does\.Contain/' $f
+	perl -i -pe 's/Is\.StringMatching/Does\.Match/' $f
+done
 find -iname '*.bak' | xargs -r rm
